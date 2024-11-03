94b301ff831d63a3fbdf83e0a00a69709d301e73
luanp-eaut <luanp@eaut.edu.vn>
2024-11-03 14:24:19 +0700

    first commit

---
+++ app/src/main/java/eaut/it/mobileappdev/volleydemo/MainActivity.java
@@ -0,0 +1,105 @@
+package eaut.it.mobileappdev.volleydemo;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button xmlButton, jsonButton, imageButton;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.textView);
+        xmlButton = findViewById(R.id.buttonXml);
+        jsonButton = findViewById(R.id.buttonJson);
+        imageButton = findViewById(R.id.buttonImage);
+        imageView = findViewById(R.id.imageView);
+
+        String xmlUrl = "https://www.w3schools.com/xml/note.xml";
+        String jsonUrl = "https://filesamples.com/samples/code/json/sample2.json";
+        String imageUrl = "https://cdn.pixabay.com/photo/2022/12/02/16/52/the-path-7631282_1280.jpg";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        xmlButton.setOnClickListener(view -> {
+            StringRequest stringRequest = getXmlStringRequest(xmlUrl);
+            requestQueue.add(stringRequest);
+        });
+
+        jsonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JsonObjectRequest jsonObjectRequest = getJsonObjectRequest(jsonUrl);
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageRequest imageRequest = getImageRequest(imageUrl);
+                requestQueue.add(imageRequest);
+            }
+        });
+    }
+
+    private StringRequest getXmlStringRequest(String url) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> textView.setText(response), error -> textView.setText(error.toString()));
+        return stringRequest;
+    }
+
+    private JsonObjectRequest getJsonObjectRequest(String url) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> textView.setText(response.toString()), error -> textView.setText(error.toString()));
+        return jsonObjectRequest;
+    }
+
+    private ImageRequest getImageRequest(String url) {
+        int max_height = 1500;
+        int max_width = 1500;
+
+        ImageRequest imageRequest = new ImageRequest(url, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                textView.setText("Photo from pixabay");
+                imageView.setImageBitmap(response);
+            }
+        }, max_width, max_height, ImageView.ScaleType.CENTER, null, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText(error.toString());
+            }
+        });
+
+        return imageRequest;
+    }
+}
